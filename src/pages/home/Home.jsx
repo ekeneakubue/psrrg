a0d897966c73bd0c3aa98e1afdf0798d32cfa502
ekeneakubue <ekeneakubue@gmail.com>
2023-06-05 19:05:44 +0100

    updated

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -20,38 +20,8 @@ export default function Home() {
             </div>              
           </div>          
         </div>     
-      </div>
+      </div>      
       
-      <div className="focus-section">
-        <div class="row">
-          <div class="col-sm-6">          
-            <div class="card text-white bg-dark mb-3">
-              <h5 class="card-header text-center">Main Research Focus and Other Areas of Research Interest of the RG</h5>
-              <div class="card-body">
-                <ul>
-                  <li>Engender pragmatic Reforms through research in the civil services at all the tiers and level of government including public enterprises and agencies</li>
-                  <li>Human resources management</li>
-                  <li>Human Capital Development</li>
-                  <li>Perfomance Management</li>
-                  <li>Programme Management</li>
-                  <li>Productivity and Service Delivery</li>
-                </ul>
-              </div>
-            </div> 
-            <br/>       
-          </div>
-          <div class="col-sm-6">            
-            <div class="card bg-light mb-3">
-              <h5 class="card-header">Special title treatment</h5>
-              <div class="card-body">
-                <h5 class="card-title">Light card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-            </div>        
-          </div>
-        </div>
-      </div>
-
       {/* About Section */}
       <div className="col-md-12">
         <div className="sub-title">Research Coordinator</div><br/>
@@ -82,8 +52,39 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      <div className="focus-section">
+        <div class="row">
+          <div class="col-sm-6">          
+            <div class="card text-white bg-dark mb-3">
+              <h5 class="card-header text-center">Main Research Focus and Other Areas of Research Interest of the RG</h5>
+              <div class="card-body">
+                <ul>
+                  <li>Engender pragmatic Reforms through research in the civil services at all the tiers and level of government including public enterprises and agencies</li>
+                  <li>Human resources management</li>
+                  <li>Human Capital Development</li>
+                  <li>Perfomance Management</li>
+                  <li>Programme Management</li>
+                  <li>Productivity and Service Delivery</li>
+                </ul>
+              </div>
+            </div> 
+            <br/>       
+          </div>
+          <div class="col-sm-6">            
+            <div class="card bg-light mb-3">
+              <h5 class="card-header">Special title treatment</h5>
+              <div class="card-body">
+                <h5 class="card-title">Light card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>        
+          </div>
+        </div>
+      </div>
+      
     </div>      
-    <div className="scholar">
+    <div className="home-scholar">
       <div className='scholar-left'>ARE YOU INTERESTED IN OUR SCHOLARSHIP SCHEME?</div> 
       <Link to = 'scholarship'><div className='btn btn-light scholar-right'>Read More...</div></Link>      
     </div>
--- src/pages/scholarships/Scholarships.jsx
+++ src/pages/scholarships/Scholarships.jsx
@@ -2,6 +2,22 @@ import './scholarships.css'
 
 export default function Scholarships() {
   return (
-    <div>Scholarships</div>
+    <div className='scholar'>
+      <div className="container">
+        <div className="scholarship">
+          <h1>Our Scholarships</h1>
+          <i className="bi bi-mortarboard-fill hat-icon"></i>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum unde 
+            necessitatibus, quasi perspiciatis beatae minima autem porro earum quos ab 
+            cum labore doloribus, vero eos alias officia ad, soluta expedita. Lorem ipsum 
+            dolor sit amet, consectetur adipisicing elit. Laborum unde necessitatibus, quasi 
+            perspiciatis beatae minima autem porro earum quos ab cum labore doloribus, vero 
+            eos alias officia ad, soluta expedita.
+          </p><br/><br/>
+
+          <h5><button type="button" class="btn btn-outline-light">Start Your Application</button></h5>
+        </div>
+      </div>
+    </div>
   )
 }
