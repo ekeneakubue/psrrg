242a5691260f5a1bc675c7d0bc18b3a7d76ed80a
Ekene Akubue <ekeneakubue@gmail.com>
2023-07-14 11:45:27 +0100

    Update Scholarships.jsx

--- src/pages/scholarships/Scholarships.jsx
+++ src/pages/scholarships/Scholarships.jsx
@@ -13,7 +13,7 @@ export default function Scholarships() {
           <p>The Association of Nsukka Professor (ANP) offers scholarship to indigent
             undergraduates of Nsukka Cultural Zone of Isi-uzo, Udenu, Igbo-Eze North, Igbo-Eze
             South, Nsukka, Igbo-Etiti and Uzo-Uwani Local Government Areas of Enugu State.
-            Objective: The purpose of the ANP scholarship scheme is to support outstanding but
+            <br/>Objective: The purpose of the ANP scholarship scheme is to support outstanding but
             indigent students from Nsukka cultural zone pursuing degree programme in any
             discipline in Nigeria Federal State Universities.             
           </p><br/>
